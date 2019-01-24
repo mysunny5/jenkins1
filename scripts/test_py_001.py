fde9e05526e8fa4cac87cd9a41a8eca27fe57733
lisi <lisi@139.com>
2019-01-24 19:01:21 +0800

    aaa

---
+++ scripts/test_py_001.py
@@ -0,0 +1,11 @@
+import allure
+@allure.step(title="测试登录的流程")
+def test_login():
+    allure.attach("登录","输入用户名")
+    print("aaa")
+    allure.attach('登录',"输入密码")
+    print("bbb")
+    allure.attach("登录","点击登录")
+    print("cccc")
+    assert 1
+
